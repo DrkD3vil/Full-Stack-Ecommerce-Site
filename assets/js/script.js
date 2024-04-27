bde55d5c558d8c4f03e2ceae56838aaeb6fa5f5f
BIJOY DEY <96878348+DrkD3vil@users.noreply.github.com>
2024-04-27 16:53:54 +0600

    First Commit
    Sidebar CSS and JS Code

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Sidebar
+
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", ()=> {
+    sidebar.classList.toggle("open")
+    menuBtnChange();
+});
+
+searchBtn.addEventListener("click", ()=> {
+    sidebar.classList.toggle("open")
+    menuBtnChange();
+});
+
+function menuBtnChange(){
+    if(sidebar.classList.contains("open")){
+        closeBtn.classList.replace("bx-menu","bx-menu-alt-right");
+    }else{
+        loseBtn.classList.replace("bx-menu-alt-right","bx-menu");
+    }
+}
